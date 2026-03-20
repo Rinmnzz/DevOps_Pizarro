aa2bb9f73107fe6b33887187a3852fb983f5d426
unknown <SSDD@POAPCA02LC0207.duoc.acad>
2026-03-20 20:08:57 -0300

    arreglo appjs

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hola Mundo\n');
+});
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Aplicación ejecutándose en http://0.0.0.0:${PORT}`);
+});
+
+
 console.log("🚀 Aplicación ejecutándose correctamente");
